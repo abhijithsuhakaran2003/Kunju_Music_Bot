a6ffc06a261c2e9c3d881b3781967836fb4e5e21
abhijithsuhakaran2003 <77870822+abhijithsuhakaran2003@users.noreply.github.com>
2021-03-29 10:27:50 +0530

    Create misc.py

---
+++ misc.py
@@ -0,0 +1,16 @@
+HELP_TEXT = """__**I Can Sing In your group Voice Chat.Start me to sing**__
+
+**/start** __To Start The bot.__
+**/help** __To Show This Message.__
+**/skip** __To Skip The Current Playing Music.__
+**/play** __<youtube/saavn/deezer> <Song_Name>__
+**/joinvc** __To Join A Voice Chat.__
+**/leavevc** __To Leave A Voice Chat.__
+**/telegram** __To Play From Telegram Audio.__
+
+__**NOTE: Do Not Assign These Commands To Bot Via BotFather.**__"""
+
+START_TEXT = "__**Hi I'm Telegram Voice Chat Bot. Join @InfoHubKerala For Support.**__"
+
+REPO_TEXT = "[Github](https://github.com/abhijithsudhakaran2003/Kunju_Music_Bot)" \
+            + " | [Group](t.me/InfoHubKerala)"
